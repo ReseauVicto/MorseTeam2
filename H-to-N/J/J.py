738aef3e101bfc6b8a6df23477d9c64b352fc599
SebLaroche <slaroche641@gmail.com>
2018-02-26 14:55:13 -0500

    Revert "Revert "HaN""
    This reverts commit 5206315af537b301d14dd6b5ca96c064562cc351.

---
+++ H-to-N/J/J.py
@@ -0,0 +1,23 @@
+from gpiozero import LED
+led = LED(17)
+
+
+led.on()
+sleep(0.25)
+led.off()
+sleep(0.25)
+
+led.on()
+sleep(0.5)
+led.off()
+sleep(0.25)
+
+led.on()
+sleep(0.5)
+led.off()
+sleep(0.25)
+
+led.on()
+sleep(0.5)
+led.off()
+sleep(0.25)
